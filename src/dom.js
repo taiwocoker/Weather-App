e68b85c9fe1a73e1e72394bfede654ae8d6cd7fc
taiwocoker <taiwofcoker@gmail.com>
2020-09-08 17:22:43 +0100

    Fix bug on dom

--- src/dom.js
+++ src/dom.js
@@ -1,28 +1,26 @@
-const Dom = _=> {
-      const location = document.getElementById('w-location');
-      const desc = document.getElementById('w-desc');
-      const string = document.getElementById('w-string');
-      const details = document.getElementById('w-details');
-      const icon = document.getElementById('w-icon');
-      const humidity = document.getElementById('w-humidity');
-      const feelsLike = document.getElementById('w-feels-like');
-      const pressure = document.getElementById('w-pressure');
-      const wind = document.getElementById('w-wind');
-  
-
-
-    const updateDom =  (weather) => {
-      location.textContent = weather.name;
-      desc.textContent = weather.weather[0].description;
-      string.innerHTML = `${Math.floor(weather.main.temp)}<span>&#xb0;</span>`;
-      icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
-      humidity.textContent = `Relative Humidity: ${weather.main.humidity}%`;
-      feelsLike.textContent = `Feels Like: ${weather.main.feels_like}`;
-      pressure.textContent = `Pressure: ${weather.main.pressure}pascal`;
-      wind.textContent = `Wind: ${weather.wind.speed}Km/h`;
+class Dom {
+    constructor() {
+      this.location = document.getElementById('w-location');
+      this.main = document.getElementById('w-main');
+      this.string = document.getElementById('w-string');
+      this.details = document.getElementById('w-details');
+      this.icon = document.getElementById('w-icon');
+      this.min_temp = document.getElementById('w-temp_min');
+      this.max_temp = document.getElementById('w-temp_max');
+      this.feelsLike = document.getElementById('w-feels-like');
+      this.wind = document.getElementById('w-wind');
     }
-
-    return updateDom
+  
+    updateDom(weather) {
+      this.location.textContent = weather.name;
+      this.main.textContent = weather.weather[0].main;
+      this.string.innerHTML = `${Math.floor(weather.main.temp)}<span>&#xb0;</span>`;
+      this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+      this.min_temp.innerHTML = `Minimum Temp: ${weather.main.temp_min}<span>&#xb0;</span>C`;
+      this.max_temp.innerHTML = `Maximum Temp: ${weather.main.temp_max}<span>&#xb0;</span>C`;
+      this.feelsLike.textContent = `Feels Like: ${weather.main.feels_like}`;
+      this.wind.textContent = `Wind: ${weather.wind.speed}Km/h`;
+    }
   }
   
   export default Dom;
